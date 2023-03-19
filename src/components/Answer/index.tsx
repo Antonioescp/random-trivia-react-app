e0f1bba163e8ed153ef0f6b2f4cbcf25e3fa48f5
Antonioescp <packageinstaller2@gmail.com>
2023-03-19 11:37:44 -0600

    Added Answer componenet

---
+++ src/components/Answer/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import './Answer.css';
+
+interface AnswerProps {
+    content: string;
+    onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const AnswerComponent: FC<AnswerProps> = ({ content, onClick }) => {
+    return <p onClick={onClick} className="answer">{content}</p>;
+}
+
+export default AnswerComponent;
